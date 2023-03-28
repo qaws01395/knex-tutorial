3cdf71851c5b16c0db006d5727babd49188e4aa6
張涵舒 <hanshuchang@oetn.cf>
2023-03-28 15:32:36 +0800

    First commit for knex tutorial

---
+++ db/knexfile.js
@@ -0,0 +1,59 @@
+const path = require('path');
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, 'db.sqlite3')
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
