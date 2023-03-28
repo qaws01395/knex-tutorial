fe796009a1acf0a47e0f0f626aacfb3574016c1e
張涵舒 <hanshuchang@oetn.cf>
2023-03-28 17:24:02 +0800

    Run seed files
    Fix some column name bug, run seed files
    ```
    ❯ knex seed:run --knexfile db/knexfile.js                                                                                                         ─╯
    Working directory changed to ~/knex-tutorial/db
    Using environment: development
    Ran 2 seed files
    ```
    Check the roles table is inserted data.

--- db/seeds/roles.js
+++ db/seeds/roles.js
@@ -6,8 +6,8 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("roles").del();
   await knex("roles").insert([
-    { id: 1, colName: "user" },
-    { id: 2, colName: "admin" },
-    { id: 3, colName: "ohmygod" },
+    { id: 1, name: "user" },
+    { id: 2, name: "admin" },
+    { id: 3, name: "ohmygod" },
   ]);
 };
