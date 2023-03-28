af3a3db0501e16b6f2e38af2607e5e5db6751b08
張涵舒 <hanshuchang@oetn.cf>
2023-03-28 16:21:53 +0800

    Add migration file for adding tables
    Migration file added.
    The migration file will add `roles` and `user_roles` tables.
    
    Command run:
    ```
    ❯ knex migrate:make create_authentication --knexfile db/knexfile.js                                                                               ─╯
    Working directory changed to ~/knex-tutorial/db
    Using environment: development
    Using environment: development
    Using environment: development
    Created Migration: /Users/hschang/knex-tutorial/db/migrations/20230328074313_create_authentication.js
    ```
    And renamed it to `xxx_create_roles.js`

---
+++ db/migrations/20230328074313_create_roles.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("roles", function (table) {
+      table.increments("id");
+      table.string("name", 255).notNullable();
+    })
+    .createTable("user_roles", function (table) {
+      table.increments(), table.integer();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("roles").dropTableIfExists("user_roles");
+};
